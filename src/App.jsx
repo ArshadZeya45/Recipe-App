027a34cdf25186daf365d045009160408c5d1cd3
ArshadZeya45 <zeyaarshad45@gmail.com>
2025-08-25 18:22:30 -0700

    Fix: MainRoutes import case issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import MainRoutes from "./routes/MainRoutes";
+import Mainroutes from "./routes/Mainroutes";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className=" bg-[#1E1E1E] text-[#F5F5F5]">
-      <MainRoutes></MainRoutes>
+      <Mainroutes></Mainroutes>
     </div>
   );
 };
